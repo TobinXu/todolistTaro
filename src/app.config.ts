34752b2abf3aeb2b03901009b0be6b45f9117b33
DESKTOP-INRNGM0\Administrator <864339010@qq.com>
2021-04-13 14:00:34 +0800

    finish todolist develop

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 export default {
   pages: [
     'pages/index/index'
-  ],
+    ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,81 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { Component, useState } from 'react'
+import { View, Text, Input, Button, EventProps, Checkbox, Label, CheckboxGroup } from '@tarojs/components'
+import { InputProps } from '@tarojs/components/types/Input'
+
 import './index.scss'
+import { CheckboxGroupProps } from '@tarojs/components/types/CheckboxGroup'
 
-export default class Index extends Component {
 
-  componentWillMount () { }
+export default () => {
+  const [newTodoText, setNewTodoText] = useState('')
+  const [todoList, setTodoList] = useState<{
+    text: string,
+    isComplete: boolean
+  }[]>([])
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
+  const handleInput: InputProps['onInput'] = e => {
+    setNewTodoText(e.detail.value)
   }
+  const handleCLick: EventProps['onClick'] = () => {
+    setTodoList(prevList => {
+      return [...prevList, {
+        text: newTodoText,
+        isComplete: false
+      }]
+    })
+    setNewTodoText('')
+  }
+  const handleCheck: CheckboxGroupProps["onChange"] = (e) => {
+    setTodoList(prevList => {
+      return prevList.map(item => {
+        return {
+          text: item.text,
+          isComplete: e.detail.value.indexOf(item.text) > -1
+        }
+      })
+    })
+  }
+  return (
+    <View>
+      <View>
+      <CheckboxGroup onChange={handleCheck}>
+        {todoList.map(item => {
+          return (
+            <View key={item.text}>
+              <Label className={item.isComplete ? 'complete' : ''}>
+              <Checkbox value={item.text} />
+              <Text>{item.text}</Text>
+              </Label>
+            </View>
+          )
+        })}
+        </CheckboxGroup>
+      </View>
+      <Input value={newTodoText} onInput={handleInput} className="input"></Input>
+      <Button onClick={handleCLick}>add todo</Button>
+    </View>
+  )
 }
+
+
+// export default class Index extends Component {
+
+
+  // componentWillMount () { }
+
+  // componentDidMount () { }
+
+  // componentWillUnmount () { }
+
+  // componentDidShow () { }
+
+  // componentDidHide () { }
+
+//   render () {
+//     return (
+//       <View className='index'>
+//         <Text>Hello world!</Text>
+//       </View>
+//     )
+//   }
+// }
